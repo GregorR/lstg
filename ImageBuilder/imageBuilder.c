593787da97cbf215f14f85d14a5c3440cb06d117
Gregor Richards <hg-yff@gregor.im>
2020-12-22 01:34:55 -0500

    Time to join the 21st century of Smalltalk syntax: :=

--- ImageBuilder/imageBuilder.c
+++ ImageBuilder/imageBuilder.c
@@ -1389,7 +1389,7 @@ int parseExpression(void)
   if(isIdentifierChar(*p))
   {
     readIdentifier();
-    if((*p == '<') && (*(p + 1) == '-'))
+    if((*p == ':') && (*(p + 1) == '='))
     {
       p++;
       p++;
